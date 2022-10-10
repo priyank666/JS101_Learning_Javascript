09dc4f7b0f418450dc823a5695a258be0f0c2010
priyank666 <priyankgautam670@gmail.com>
2022-10-10 19:00:44 +0530

    add file

---
+++ Day-1/problem4.js
@@ -0,0 +1,20 @@
+let name="Priyank Gautam";
+let school="S.S.V.I. College";
+let grade="A";
+let section="B";
+let rollno="66";
+let score="marks";
+let english="90";
+let math="95";
+let science="98";
+
+console.log(name);
+console.log(school);
+console.log(grade);
+console.log(section);
+console.log(rollno);
+console.log(score);
+console.log(english);
+console.log(math);
+console.log(science);
+
---
+++ Day-3/problem3.js
@@ -0,0 +1,10 @@
+// Rahul purchased items worth 4000, there is deal of 10% discount if the bill is more than or equal to 3900, print the new bill amount if he is eligible else print "Not eligible".
+
+let num= 10;
+
+
+if (num%5==0){
+     console.log("Multiple by 5");  
+}else if (num % 2 == 0){
+  console.log("Multiple by 5");
+}
